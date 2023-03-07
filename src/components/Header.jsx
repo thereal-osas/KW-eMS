f3a40bce67f5f97ed9c68d3467772e4879d09a8b
khalil <salamikhalil02@gmail.com>
2023-03-07 23:53:32 +0100

    Project starter
    Project starter

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+	return <div className="text-blue-500 text-5xl">Header</div>;
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+function Home() {
+	return (
+		<>
+			<Navbar />
+			<Header />
+		</>
+	);
+}
+
+export default Home;
