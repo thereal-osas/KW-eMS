9100f8fe9452c60c8619df43e0d9741ef67e8ac7
khalil <salamikhalil02@gmail.com>
2023-03-08 17:53:05 +0100

    Reps page and News page update

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,7 +1,357 @@
 import React from "react";
 
 function News() {
-	return <div>News</div>;
+	return (
+		<>
+			<section>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut ad eveniet
+				excepturi, nostrum, perferendis, distinctio praesentium cupiditate dicta
+				quod quaerat voluptatibus sequi corporis labore deserunt aliquam unde
+				quasi voluptas aspernatur. Nam aliquam nulla cupiditate placeat deleniti
+				quae repudiandae ipsum reiciendis impedit eaque molestiae, debitis
+				repellendus id, libero porro expedita, voluptas earum unde dolores rem
+				maxime similique quos! Labore ducimus sint ullam fugiat sequi?
+				Voluptates sequi voluptas illo repudiandae explicabo accusantium,
+				tempora voluptatem velit error illum ullam nisi minus veniam debitis
+				officia numquam quaerat eveniet suscipit hic. Et suscipit doloribus
+				itaque accusantium earum delectus fugit? Dolorum quia ducimus magni
+				pariatur inventore.
+			</section>
+			<section className="flex gap-4 flex-col lg:flex-row px-4">
+				<div className="w-full lg:w-3/5 ">
+					<p className="font-semibold underline hidden lg:flex p-2 ">
+						Latest News
+					</p>
+					<main className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-2 text-sm">
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+						<div>
+							<div className="relative   ">
+								<img
+									className=""
+									src="https://media.premiumtimesng.com/wp-content/files/2022/10/20220528_233007.jpg"
+									alt=""
+								/>
+								<p className="absolute text-white text-sm bottom-0 bg-black/60 p-2 z-10">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Nostrum, veritatis. Saepe veritatis ea fugit quibusdam libero?
+									Veniam aspernatur ut impedit!
+								</p>
+							</div>
+							<div className="border-2 border-gray-700 px-2 rounded-b-xl">
+								<div className="flex justify-between text-sm font-semibold px-4 pt-2 ">
+									<span>By Caleb</span>
+									<span>Feb 5,2023</span>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+									Dolorem accusantium accusamus aspernatur, ex eaque excepturi
+									blanditiis, ipsa dicta temporibus, provident laborum quas cum
+									fuga amet consequuntur libero dolore assumenda voluptates
+									quisquam autem hic magnam aliquid! Libero et quas animi
+									adipisci.
+								</p>
+							</div>
+						</div>
+					</main>
+				</div>
+				<p className="font-semibold underline lg:hidden flex  ">
+					Trending Headlines
+				</p>
+
+				<div className="lg:w-2/5 w-full lg:flex flex-col grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-2  md:gap-2">
+					<p className="font-semibold underline hidden lg:flex  ">
+						Trending Headlines
+					</p>
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+					<div className=" lg:border-none border-2 rounded-xl px-3 border-black">
+						<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+						<p className="text-green-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Doloremque, corporis est adipisci deleniti atque deserunt ad
+							blanditiis sapiente eaque earum.
+						</p>
+						<div className="flex justify-between">
+							<span>By Caleb</span>
+							<span>Feb 5,2023</span>
+						</div>
+					</div>
+					<hr className="border-black border-1 lg:flex hidden " />
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default News;
--- src/pages/Reps.jsx
+++ src/pages/Reps.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import Img3 from "../assets/HOD3.jpg";
+import Img2 from "../assets/HOD2.jpg";
+import Img4 from "../assets/HOD4.jpg";
+import cook from "../assets/Abdulganiyu-Cook-Olododo.jpg";
 
 function Reps() {
-	return <div>Reps</div>;
+	return (
+		<div>
+			<p className="text-xl font-semibold underline underline-offset-1 p-4 text-center">
+				HOUSE OF REPS CANDIDATES
+			</p>
+			<div className=" mx-auto w-fit grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+				<div className="max-w-[280px] flex flex-col justify-center gap-3">
+					<img
+						className=""
+						src={cook}
+						alt="Image1"
+					/>
+					<div className="border-2 text-center">
+						<p className="font-semibold">Abdulganiyu Cook Olododo</p>
+						<p className="font-semibold">APC</p>
+					</div>
+				</div>
+				<div className="max-w-[280px] flex flex-col justify-center gap-3">
+					<img
+						className=""
+						src={Img2}
+						alt="Image2"
+					/>
+					<div className="border-2 text-center">
+						<p className="font-semibold"> Consectetur Adipisicing </p>
+						<p className="font-semibold">Tyop</p>
+					</div>
+				</div>
+				<div className="max-w-[280px] flex flex-col justify-center gap-3">
+					<img
+						className=""
+						src={Img3}
+						alt="Image4"
+					/>
+					<div className="border-2 text-center">
+						<p className="font-semibold"> Consectetur Adipisicing </p>
+						<p className="font-semibold">Tyop</p>
+					</div>
+				</div>
+				<div className="max-w-[280px] flex flex-col justify-center gap-3">
+					<img
+						className=""
+						src={Img4}
+						alt="Image4"
+					/>
+					<div className="border-2 text-center">
+						<p className="font-semibold"> Consectetur Adipisicing </p>
+						<p className="font-semibold">Tyop</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Reps;
