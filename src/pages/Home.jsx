fee45be15da791bb3f3c7dd4323da7eb5efe5840
thereal-osas <85516194+thereal-osas@users.noreply.github.com>
2023-03-10 11:29:38 +0100

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react'
+/* import CandidatesWatch from '../components/CandidatesWatch' */
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import InteractiveMap from '../components/InteractiveMap'
+import LiveResult from '../components/LiveResult'
+import NewsArticles from '../components/NewsArticles'
+import Footer from '../components/Footer'
 
 function Home() {
-	return (
-		<>
-			<p>Home</p>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className='bg-[#f4f7ff]'>
+        <Hero />
+        <LiveResult />
+        <InteractiveMap />
+        {/* <CandidatesWatch /> */}
+        <NewsArticles />
+        <Footer />
+      </div>
+    </>
+  )
 }
 
-export default Home;
+export default Home
