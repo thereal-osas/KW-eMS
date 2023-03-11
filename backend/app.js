55ac32c7169bf0b6c755a8863d0fa3526af094cd
Banji <87107632+Banji-07@users.noreply.github.com>
2023-03-12 00:51:44 +0100

    Connection to MongoDb cluster has been successfully implemented.

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const lgaControllers = require("./controllers/LocalGovernmentAreaController");
 const wardControllers = require("./controllers/WardController");
 const pollingControllers = require("./controllers/PollingUnitController");
-
 const app = express();
 
 //middleware
@@ -12,13 +11,18 @@ app.use(express.json()); // parses the json content type...
 //database connection
 const PORT = process.env.PORT;
 const DBURI = process.env.DBURI;
-
-mongoose.connect(DBURI,
-    {useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true}, () => {
+mongoose.set('strictQuery', true);
+mongoose.connect(DBURI, {useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true})
+    .then(instance => {
+        // console.log(instance);
         app.listen(PORT, () => {
-            console.log(` Connected to MongoDb...server is running on PORT: ${PORT}`)
+            console.log(` Connected to MongoDb...server is running on PORT: ${PORT}`);
         })
-    });
+    }).catch(error => {
+    console.log(error);
+});
+
+
 let baseUrl = "/Kwara";
 app.get(`${baseUrl}/lga/retrieve-identifiers`, lgaControllers.getLgaIdentifiers);
 app.get(`${baseUrl}/lga/retrieve-ward-identifiers`, lgaControllers.retrieveWardIdentifiers);
