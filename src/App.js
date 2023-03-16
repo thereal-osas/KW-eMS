46b18988ccfed9225f60073351544fea2e0eba48
Osas <abrahamosas3@gmail.com>
2023-03-16 13:36:15 +0100

    Resolved some conflicts with the pull request for interactive map

--- src/App.js
+++ src/App.js
@@ -1,68 +1,46 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense } from 'react'
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Loader from "./components/loader";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Loader from './components/loader'
 
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 // import Gubernatorial from "./pages/Gubernatorial";
-import Home from "./pages/Home";
-import Error from "./pages/Error";
-import LiveResult from "./pages/LiveResult";
-import News from "./pages/News";
+import Home from './pages/Home'
+import Error from './pages/Error'
+import LiveResult from './pages/LiveResult'
+import News from './pages/News'
+import PresidentialMap from './pages/PresidentialMap'
 // import Presidential from "./pages/Presidential";
 // import Reps from "./pages/Reps";
 // import Senatorial from "./pages/Senatorial";
-const Presidential = lazy(() => import("./pages/Presidential"));
-const Gubernatorial = lazy(() => import("./pages/Gubernatorial"));
-const Senatorial = lazy(() => import("./pages/Senatorial"));
-const Reps = lazy(() => import("./pages/Reps"));
+const Presidential = lazy(() => import('./pages/Presidential'))
+const Gubernatorial = lazy(() => import('./pages/Gubernatorial'))
+const Senatorial = lazy(() => import('./pages/Senatorial'))
+const Reps = lazy(() => import('./pages/Reps'))
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			<Suspense fallback={<Loader />}>
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/live_results"
-						element={<LiveResult />}
-					/>
-					<Route
-						path="/news"
-						element={<News />}
-					/>
-					<Route path="/candidates">
-						<Route
-							path="presidential"
-							element={<Presidential />}
-						/>
-						<Route
-							path="senatorial"
-							element={<Senatorial />}
-						/>
-						<Route
-							path="gubernatorial"
-							element={<Gubernatorial />}
-						/>
-						<Route
-							path="house_of_reps"
-							element={<Reps />}
-						/>
-					</Route>
-					<Route
-						path="*"
-						element={<Error />}
-					/>
-				</Routes>
-			</Suspense>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/live_results' element={<LiveResult />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/candidates'>
+            <Route path='presidential' element={<Presidential />} />
+            <Route path='senatorial' element={<Senatorial />} />
+            <Route path='gubernatorial' element={<Gubernatorial />} />
+            <Route path='house_of_reps' element={<Reps />} />
+          </Route>
+          <Route path='/interactive_map' element={<PresidentialMap />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,94 +1,79 @@
-import React, { useState } from "react";
-import { FaTimes, FaBars } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import Navmenu from "./Navmenu";
+import React, { useState } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import Navmenu from './Navmenu'
 
 function Navbar() {
-	const [Open, setOpen] = useState(false);
+  const [Open, setOpen] = useState(false)
 
-	const Toggle = () => {
-		setOpen(!Open);
-	};
+  const Toggle = () => {
+    setOpen(!Open)
+  }
 
-	return (
-		<section className="mb-12">
-			<main className=" fixed top-0 left-0 right-0 bg-white z-50 ">
-				<div className="justify-between px-4 py-2 flex w-full">
-					<Link
-						to="/"
-						className="text-xl font-bold">
-						KWEMS
-					</Link>
-					<div className="hidden space-x-6 sm:flex items-center">
-						<Link
-							className="text-sm font-medium"
-							to="/">
-							Home
-						</Link>
-						<Navmenu />
-						{/* <Link
+  return (
+    <section className='mb-12'>
+      <main className=' fixed top-0 left-0 right-0 bg-white z-50 '>
+        <div className='justify-between px-4 py-2 flex w-full'>
+          <Link to='/' className='text-xl font-bold'>
+            KWEMS
+          </Link>
+          <div className='hidden space-x-6 sm:flex items-center'>
+            <Link className='text-sm font-medium' to='/'>
+              Home
+            </Link>
+            <Navmenu />
+            {/* <Link
 							className="text-sm font-medium"
 							to="/candidates">
 							Candidates
 						</Link> */}
-						<Link
-							className="text-sm font-medium"
-							to="/live_results">
-							Live Results
-						</Link>
-						<Link
-							className="text-sm font-medium"
-							to="/news">
-							News
-						</Link>
-					</div>
-					<div>
-						<button className="bg-[#336DFF] text-white px-4 py-1 rounded-2xl">
-							Interactive Map
-						</button>
-					</div>
-					<button
-						className="flex sm:hidden duration-1000 "
-						onClick={Toggle}>
-						{Open ? <FaTimes size={25} /> : <FaBars size={25} />}
-					</button>
-				</div>
-				<hr className="border-b-2  border-black" />
-			</main>
-			<aside>
-				<div
-					className={`z-10 duration-1000 p-10  fixed top-10 left-0 right-0 bottom-0 ${
-						Open ? "bg-[#336DFF] " : " bg-[#336DFF] translate-x-full "
-					}`}>
-					<div className="flex space-y-10 sm:hidden flex-col justify-center items-center text-white">
-						<div className="sm:hidden flex flex-col items-center  justify-around h-[80vh]  text-2xl gap-4">
-							<Link
-								className=""
-								to="/">
-								Home
-							</Link>
-							{/* <Navmenu /> */}
-							<Link
-								className=""
-								to="/candidates">
-								Candidates
-							</Link>
-							<Link
-								className=""
-								to="/live_results">
-								Live Results
-							</Link>
-							<Link
-								className=""
-								to="/news">
-								News
-							</Link>
-						</div>
-					</div>
-				</div>
-			</aside>
-		</section>
-	);
+            <Link className='text-sm font-medium' to='/live_results'>
+              Live Results
+            </Link>
+            <Link className='text-sm font-medium' to='/news'>
+              News
+            </Link>
+          </div>
+          <div>
+            <Link to='/interactive_map'>
+              <button className='bg-[#336DFF] text-white px-4 py-1 rounded-2xl'>
+                Interactive Map
+              </button>
+            </Link>
+          </div>
+          <button className='flex sm:hidden duration-1000 ' onClick={Toggle}>
+            {Open ? <FaTimes size={25} /> : <FaBars size={25} />}
+          </button>
+        </div>
+        <hr className='border-b-2  border-black' />
+      </main>
+      <aside>
+        <div
+          className={`z-10 duration-1000 p-10  fixed top-10 left-0 right-0 bottom-0 ${
+            Open ? 'bg-[#336DFF] ' : ' bg-[#336DFF] translate-x-full '
+          }`}
+        >
+          <div className='flex space-y-10 sm:hidden flex-col justify-center items-center text-white'>
+            <div className='sm:hidden flex flex-col items-center  justify-around h-[80vh]  text-2xl gap-4'>
+              <Link className='' to='/'>
+                Home
+              </Link>
+              {/* <Navmenu /> */}
+              <Link className='' to='/candidates'>
+                Candidates
+              </Link>
+              <Link className='' to='/live_results'>
+                Live Results
+              </Link>
+              <Link className='' to='/news'>
+                News
+              </Link>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </section>
+  )
 }
 
-export default Navbar;
+export default Navbar
